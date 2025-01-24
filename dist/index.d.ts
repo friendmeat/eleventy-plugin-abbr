7c3a9ecf91ebf18e7d72e20cd897d0d137bf0f40
friendmeat <friendmeat@proton.me>
2025-01-24 16:53:58 -0500

    Remove dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-type DefinitionPluginOptions = {
-    definitions: Map<string, string>;
-    caseSensitive?: boolean;
-};
-export default function (eleventyConfig: any, options: DefinitionPluginOptions): void;
-export {};
